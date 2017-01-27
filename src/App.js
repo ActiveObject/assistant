6e00bd3332a602a77ee89effdd26c0c553121afc
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2017-01-27 19:29:34 +0200

    Add y axis

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { TransitionMotion, spring } from 'react-motion';
 import './App.css';
 import { scaleTime, scaleLinear } from 'd3-scale';
 import { area, line, curveNatural } from 'd3-shape';
 import { extent } from 'd3-array';
+import { format } from 'd3-format';
 import MotionPath from './MotionPath';
 
 class App extends Component {
@@ -62,8 +64,48 @@ function Chart({ balance, width, height }) {
       <MotionPath path={createLine(balance)}>
         {d => <path d={d} fill="none" stroke="#71D1CA" strokeWidth="3" strokeLinejoin="round" strokeLinecap="round" />}
       </MotionPath>
+
+      <YAxis ticks={10} scale={y} width={width} />
     </svg>
   )
+}
+
+function YAxis({ ticks, scale, width }) {
+  const styles = scale.ticks(ticks).slice(1).map((t) => ({
+		key: String(t),
+		data: t,
+		style: {
+			x: spring(scale(t)),
+			opacity: spring(1),
+		},
+	}));
+
+  const fixedPoint = format('.3f');
+
+	return (
+		<TransitionMotion styles={styles} willLeave={willLeave} willEnter={willEnter}>
+			{(styles) => (
+				<g className="axis">
+					{styles.map(({key, data, style}) => (
+						<g key={key}
+							transform={`translate(0, ${fixedPoint(style.x)})`}
+							opacity={fixedPoint(style.opacity)}>
+
+							<text x={20} y={2} children={data} />
+						</g>
+					))}
+				</g>
+			)}
+		</TransitionMotion>
+	);
+}
+
+function willLeave({style}) {
+	return {x: style.x, opacity: spring(0)};
+}
+
+function willEnter({style}) {
+	return {x: style.x.val, opacity: 0};
 }
 
 function generateData() {
