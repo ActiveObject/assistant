657779e276be241f2abf7ec552654c17f94da2f9
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2017-01-29 21:58:57 +0200

    Show expenditure by month

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { TransitionMotion, spring } from 'react-motion';
 import './App.css';
-import { scaleTime, scaleLinear } from 'd3-scale';
+import { scaleTime, scaleLinear, scaleBand } from 'd3-scale';
 import { area, line, curveMonotoneX } from 'd3-shape';
 import { extent } from 'd3-array';
 import { format } from 'd3-format';
+import { timeFormat } from 'd3-time-format';
 import MotionPath from './MotionPath';
 import Importer from './Importer';
 
@@ -44,12 +45,57 @@ class App extends Component {
               <AccountBalanceChart transactions={transactions} width={width} height={height} range={range} />
               <input type="range" min={0} max={transactions.length - 1} step="1" value={range[0]} onChange={this.setRangeStart} />
               <input type="range" min={0} max={transactions.length - 1} step="1" value={range[1]} onChange={this.setRangeEnd} />
+              <MonthExpenditureChart transactions={transactions} width={width / 3} height={height / 3} />
             </header>
           }
         </Importer>
       </div>
     );
   }
+}
+
+function MonthExpenditureChart({ transactions, width, height }) {
+  var expenditureByMonth = new Map();
+
+  transactions.forEach(transaction => {
+    var key = timeFormat('%b %Y')(transaction.date);
+
+    if (!expenditureByMonth.has(key)) {
+      expenditureByMonth.set(key, 0);
+    }
+
+    if (transaction.amount < 0) {
+      expenditureByMonth.set(key, expenditureByMonth.get(key) - transaction.amount);
+    }
+  });
+
+  var x = scaleBand()
+    .domain([...expenditureByMonth.keys()])
+    .rangeRound([0, width])
+    .padding(0.1)
+
+  var y = scaleLinear()
+    .domain([0, Math.max(...expenditureByMonth.values())])
+    .rangeRound([height, 0]);
+
+  return (
+    <svg width={width} height={height}>
+      {[...expenditureByMonth.keys()].map(month =>
+        <rect
+          key={month}
+          fill="#9FE9E4"
+          x={x(month)}
+          y={y(expenditureByMonth.get(month))}
+          width={x.bandwidth()}
+          height={height - y(expenditureByMonth.get(month))}
+        >
+          <title>{`${month} - ${expenditureByMonth.get(month)} UAN`}</title>
+        </rect>
+      )}
+
+      <YAxis ticks={10} scale={y} width={width} />
+    </svg>
+  )
 }
 
 function AccountBalanceChart({ transactions, width, height, range }) {
